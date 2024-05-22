6b4c2a890d5179fef3c47253944532d851465f68
edsonmier <edson_mier28@hotmail.com>
2024-05-22 14:37:59 -0600

    fix: list

--- src/components/server.js
+++ src/components/server.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: "http://localhost:3001"
 }));
 
-const tiktokUsername = "lanaslifeee";
+const tiktokUsername = "dark.osciris";
 const tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
 
 // Inicializa el conjunto con los suscriptores existentes
